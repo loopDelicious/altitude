0cb928ae1658180c66e73e48fe3466dfc5bad4a5
Joyce Lin <joycelin79@gmail.com>
2020-01-31 15:41:56 -0800

    remove helloworld

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Intro/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Intro from './components/Intro.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Intro
   }
 }
 </script>
---
+++ src/components/Intro.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="intro">
+    <h3>Altitude</h3>
+    <ul>
+      <li><a href="https://neal.fun/deep-sea/" target="_blank" rel="noopener">The Deep Sea</a></li>
+      <li><a href="https://xkcd.com/482/" target="_blank" rel="noopener">height</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Intro'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
